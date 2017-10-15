59d99d8eb446cc45094c9ba0496c9c8d5f404238
yang24201 <yang24201@126.com>
2017-10-15 12:00:35 +0800

    modify client ngrok domain

--- src/util/appConfig.js
+++ src/util/appConfig.js
@@ -26,7 +26,7 @@ const BACKEND_DOMAIN_PRO = "http://share.xiaojee.cn"
 
 // 客户端域名
 var CLIENT_DOMAIN = ''
-const CLIENT_DOMAIN_DEV = 'http://dev.mp.ngrok.io'
+const CLIENT_DOMAIN_DEV = 'http://dev.mp.ngrok.lvyii.com'
 const CLIENT_DOMAIN_STAGE = 'http://dev.xiaojee.cn:6300'
 const CLIENT_DOMAIN_PRO = 'http://admin.xiaojee.cn:6300'
 
