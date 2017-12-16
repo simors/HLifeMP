f683c56cd437f97536bcca94d5bfc11d3f8e8c50
reluo <21404683@qq.com>
2017-12-16 14:46:23 +0800

    svg create

--- src/route/Mine/CreateMyAddr.js
+++ src/route/Mine/CreateMyAddr.js
@@ -8,7 +8,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
 import Avatar from '../../component/avatar'
-import {Button, WingBlank, InputItem, List,Toast} from 'antd-mobile'
+import {Button, WingBlank, InputItem, List, Toast, Icon} from 'antd-mobile'
 import {authSelector} from '../../util/auth'
 import {mineAction, mineSelector} from '../../route/Mine/redux'
 import Loading from '../../component/loading'
@@ -20,14 +20,15 @@ class CreateMyAddr extends React.PureComponent {
   constructor(props) {
     super(props)
     document.title = '创建收货地址'
-    this.state={
+    this.state = {
       showCusInput: false,
       province: undefined,
       city: undefined,
       district: undefined,
-      tag: undefined
+      tag: undefined,
+      isCus: false
     }
-    this.customFocusInst='ttt'
+    this.customFocusInst = 'ttt'
   }
 
   componentDidMount() {
@@ -40,6 +41,7 @@ class CreateMyAddr extends React.PureComponent {
 
   submit = ()=> {
     this.props.form.validateFields((error, value) => {
+
       let payload = {
         username: value.username,
         mobilePhoneNumber: value.mobilePhoneNumber,
@@ -48,27 +50,80 @@ class CreateMyAddr extends React.PureComponent {
         city: this.state.city,
         district: this.state.district,
         tag: this.state.tag,
-        success:()=>{this.props.history.goBack()},
-        error: (err)=>{Toast.fail(err.message)}
+        success: ()=> {
+          this.props.history.goBack()
+        },
+        error: (err)=> {
+          Toast.fail(err.message)
+        }
       }
-
-      this.props.createMyAddr(payload)
+      if(payload.addrId!=''&&payload.username!=''&&payload.mobilePhoneNumber!=''&&payload.addr!=''&&payload.province!=''&&payload.city!=''&&payload.district!=''&&payload.tag!=''){
+        this.props.createMyAddr(payload)
+      }else{
+        Toast.fail('请填写全部内容！')
+      }
 
       console.log(error, value);
     });
   }
 
-  renderTagBox(){
+  renderTagBox() {
     return (
       <div className={styles.tagBox}>
         <span className={styles.tagLabel}>标签:</span>
-        <div className={styles.tagWrap}>
-          <div className={this.state.tag=='家'?styles.selectedTag:styles.tag} onClick={()=>{this.setState({tag:'家'})}}>家</div>
-          <div className={this.state.tag=='公司'?styles.selectedTag:styles.tag} onClick={()=>{this.setState({tag:'公司'})}}>公司</div>
-          <div className={this.state.tag=='学校'?styles.selectedTag:styles.tag} onClick={()=>{this.setState({tag:'学校'})}}>学校</div>
+        <div className={styles.tagWrapBox}>
+          <div className={styles.tagWrap}>
+            <div className={this.state.tag == '家' ? styles.selectedTag : styles.tag} onClick={()=> {
+              this.setState({isCus: false,tag: '家', showCusInput: false})
+            }}>家
+            </div>
+            <div className={this.state.tag == '公司' ? styles.selectedTag : styles.tag} onClick={()=> {
+              this.setState({isCus: false,tag: '公司', showCusInput: false})
+            }}>公司
+            </div>
+            <div className={this.state.tag == '学校' ? styles.selectedTag : styles.tag} onClick={()=> {
+              this.setState({isCus: false,tag: '学校', showCusInput: false})
+            }}>学校
+            </div>
+          </div>
+          <div className={styles.tagWrap}>
+            <div className={this.state.isCus ? styles.selectedTag : styles.cusTag} onClick={()=> {
+              this.setState({tag:'',isCus: true, showCusInput: true}, ()=> {
+                this.handleClick()
+              })
+            }}>
+              {(this.state.isCus&&this.state.tag!='') ? this.state.tag : <Icon className={styles.tagIcon} type={require('../../asset/svg/add_tag.svg')}/>}
+            </div>
+            {this.state.showCusInput ?
+              <div className={styles.inputBox}>
+                <input ref="myInput" className={styles.input} onChange={(e)=> {
+                  this.changeTag(e)
+                }}/>
+                <div className={styles.confirm} onClick={()=> {
+                  this.setState({showCusInput: false})
+                }}>确认
+                </div>
+              </div>
+              : null}
+          </div>
         </div>
       </div>
     )
+  }
+
+  changeTag(e){
+    let text = e.target.value
+    if(text&&text.length<4){
+      this.setState({tag: text})
+    }else{
+      Toast.fail('字数不能大于3')
+    }
+  }
+
+  handleClick() {
+    // 使用原生的 DOM API 获取焦点
+    console.log('refs=============>', this.refs)
+    this.refs.myInput.focus();
   }
 
   render() {
@@ -77,30 +132,29 @@ class CreateMyAddr extends React.PureComponent {
 
     return (
       <div className={styles.body}>
-        <div className = {styles.inputBox}>
+        <div className={styles.inputBox}>
           <span className={styles.inputLabel}>收货人:</span>
-          <input className={styles.input} {...getFieldProps('username',{
+          <input className={styles.input} {...getFieldProps('username', {
             initialValue: user.nickname
           })}/>
         </div>
-        <div className = {styles.inputBox}>
+        <div className={styles.inputBox}>
           <span className={styles.inputLabel}>联系电话:</span>
-          <input className={styles.input} {...getFieldProps('mobilePhoneNumber',{
+          <input className={styles.input} {...getFieldProps('mobilePhoneNumber', {
             initialValue: user.mobilePhoneNumber
           })}/>
         </div>
-        <RegionPicker  className={styles.picker} level={3} onOk={(value)=> {
+        <RegionPicker className={styles.picker} level={3} onOk={(value)=> {
           this.setState({province: value[0], city: value[1], district: value[2]})
-        }} />
+        }}/>
 
-        <div className = {styles.inputBox}>
+        <div className={styles.inputBox}>
           <span className={styles.inputLabel}>详细地址:</span>
-          <input className={styles.input} {...getFieldProps('addr',{
+          <input className={styles.input} {...getFieldProps('addr', {
             initialValue: ''
           })}/>
         </div>
         {this.renderTagBox()}
-
 
         <div className={styles.submit} onClick={this.submit}>保存</div>
       </div>
--- src/route/Mine/UpdateMyAddr.js
+++ src/route/Mine/UpdateMyAddr.js
@@ -8,7 +8,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
 import Avatar from '../../component/avatar'
-import {Button, WingBlank, InputItem, List, Toast} from 'antd-mobile'
+import {Button, WingBlank, InputItem, List, Toast, Icon} from 'antd-mobile'
 import {authSelector} from '../../util/auth'
 import {mineAction, mineSelector} from '../../route/Mine/redux'
 import Loading from '../../component/loading'
@@ -26,14 +26,20 @@ class UpdateMyAddr extends React.PureComponent {
       province: undefined,
       city: undefined,
       district: undefined,
-      tag: undefined
+      tag: '',
+      isCus: false
     }
     this.customFocusInst = 'ttt'
   }
 
   componentWillMount() {
     let {addr} = this.props
-    this.setState({tag: addr.tag})
+    if(addr.tag=='家'||addr.tag=='公司'||addr.tag=='学校'){
+      this.setState({tag: addr.tag})
+    }else{
+      this.setState({tag: addr.tag,isCus:true})
+
+    }
 
   }
 
@@ -43,6 +49,8 @@ class UpdateMyAddr extends React.PureComponent {
 
   submit = ()=> {
     this.props.form.validateFields((error, value) => {
+
+
       let payload = {
         addrId:this.props.addr.id,
         username: value.username,
@@ -55,33 +63,75 @@ class UpdateMyAddr extends React.PureComponent {
         success: ()=>{this.props.history.goBack()},
         error: (err)=>{Toast.fail(err.message)}
       }
+      if(payload.addrId!=''&&payload.username!=''&&payload.mobilePhoneNumber!=''&&payload.addr!=''&&payload.province!=''&&payload.city!=''&&payload.district!=''&&payload.tag!=''){
+        this.props.updateMyAddr(payload)
+      }else{
+        Toast.fail('请填写全部内容！')
+      }
 
-      this.props.updateMyAddr(payload)
 
       console.log(error, value);
     });
   }
 
+
   renderTagBox() {
     return (
       <div className={styles.tagBox}>
         <span className={styles.tagLabel}>标签:</span>
-        <div className={styles.tagWrap}>
-          <div className={this.state.tag == '家' ? styles.selectedTag : styles.tag} onClick={()=> {
-            this.setState({tag: '家'})
-          }}>家
+        <div className={styles.tagWrapBox}>
+          <div className={styles.tagWrap}>
+            <div className={this.state.tag == '家' ? styles.selectedTag : styles.tag} onClick={()=> {
+              this.setState({isCus: false,tag: '家', showCusInput: false})
+            }}>家
+            </div>
+            <div className={this.state.tag == '公司' ? styles.selectedTag : styles.tag} onClick={()=> {
+              this.setState({isCus: false,tag: '公司', showCusInput: false})
+            }}>公司
+            </div>
+            <div className={this.state.tag == '学校' ? styles.selectedTag : styles.tag} onClick={()=> {
+              this.setState({isCus: false,tag: '学校', showCusInput: false})
+            }}>学校
+            </div>
           </div>
-          <div className={this.state.tag == '公司' ? styles.selectedTag : styles.tag} onClick={()=> {
-            this.setState({tag: '公司'})
-          }}>公司
-          </div>
-          <div className={this.state.tag == '学校' ? styles.selectedTag : styles.tag} onClick={()=> {
-            this.setState({tag: '学校'})
-          }}>学校
+          <div className={styles.tagWrap}>
+            <div className={this.state.isCus ? styles.selectedTag : styles.cusTag} onClick={()=> {
+              this.setState({tag:'',isCus: true, showCusInput: true}, ()=> {
+                this.handleClick()
+              })
+            }}>
+              {(this.state.isCus&&this.state.tag!='') ? this.state.tag : <Icon className={styles.tagIcon} type={require('../../asset/svg/add_tag.svg')}/>}
+            </div>
+            {this.state.showCusInput ?
+              <div className={styles.inputBox}>
+                <input ref="myInput" className={styles.input} onChange={(e)=> {
+                  this.changeTag(e)
+                }}/>
+                <div className={styles.confirm} onClick={()=> {
+                  this.setState({showCusInput: false})
+                }}>确认
+                </div>
+              </div>
+              : null}
           </div>
         </div>
       </div>
     )
+  }
+
+  changeTag(e){
+    let text = e.target.value
+    if(text&&text.length<4){
+      this.setState({tag: text})
+    }else{
+      Toast.fail('字数不能大于3')
+    }
+  }
+
+  handleClick() {
+    // 使用原生的 DOM API 获取焦点
+    console.log('refs=============>', this.refs)
+    this.refs.myInput.focus();
   }
 
   render() {
